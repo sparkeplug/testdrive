1399c9ad835793a54d4d86f039a8a7bd31ce3fef
prabhu <prabhu.rk90@gmail.com>
2020-08-31 01:16:11 +0530

    Added TDD support using JEST

---
+++ src/__tests__/LoginForm.test.tsx
@@ -0,0 +1,10 @@
+import LoginForm, { Props } from "../components/LoginForm";
+import { fireEvent, render, waitForElement } from "@testing-library/react";
+
+import React from "react";
+
+describe("<LoginForm />", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    // ???
+  });
+});
